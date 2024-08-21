8e4b7425b5d2829bca42fbfd126f235682aff8dc
Pawan-Mario <mauryapawan002@gmail.com>
2024-08-21 23:05:09 +0530

    venv file updated

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Rohit Maurya\OneDrive\Desktop\python\Ganpati_pro\Bappa\venv
+set VIRTUAL_ENV=C:\Users\Rohit Maurya\OneDrive\Desktop\python\Ganpati_pro 1\Bappa\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
